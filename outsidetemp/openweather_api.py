8a958d8c1a0bb3bdf90ce28cd2f29218c642f60e
Richard Dubois <dubois.richard@gmail.com>
2019-12-21 01:43:34 +0000

    minor change

--- outsidetemp/openweather_api.py
+++ outsidetemp/openweather_api.py
@@ -21,6 +21,29 @@ import sys
 import logging
 
 
+# create logger with 'openweather_application'
+logger = logging.getLogger('openweather_application')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('openweather.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+app = Flask(__name__)
+api = Api(app)
+
+import socket
+
 
 class OutsideTemp(Resource):
 
