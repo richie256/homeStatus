5441eaccf5af2333dece529323fbb43b3b3e9f72
Richard Dubois <dubois.richard@gmail.com>
2020-01-16 21:37:03 -0500

    WIP to have mqtt exporter

--- rtlamr-mqttexporter/rtlamr-mqttexporter.py
+++ rtlamr-mqttexporter/rtlamr-mqttexporter.py
@@ -2,6 +2,8 @@ import json
 
 import docker
 
+import datetime
+
 import os
 
 # from prometheus_client import start_http_server, Gauge
@@ -34,6 +36,21 @@ def process(current_line):
     except json.decoder.JSONDecodeError:
         return
 
+    # Exemple: "Time": "2020-01-13T02:53:46.093844126Z"
+    nanoseconds = data['Time'].split(".")[1]
+    millisecondstz = nanoseconds[:6] + nanoseconds[9:]
+    datetimestr = data['Time'].split(".")[0] + '.' + millisecondstz
+
+    dt = datetime.datetime.strptime(datetimestr, '%Y-%m-%dT%H:%M:%S.%f%Z')
+
+    current_datetime = datetime.datetime.utcnow()
+
+    delta = current_datetime - dt
+
+    # We ignore logs more than minute
+    if delta.minutes > 2:
+      return
+
     # get some required info: current meter reading, current interval id, most recent interval usage
     # read_cur = int(flds[15])
     # interval_cur = int(flds[10])
@@ -53,7 +70,7 @@ def process(current_line):
 
     # current_reading_in_kwh = (read_cur * settings.WH_MULTIPLIER) / 1000
 
-    reading_delta = get_delta(endpoint_id)
+    reading_delta = get_delta(endpoint_id, consumption)
 
     # sensors/gas/1234567/total_consumption = 968922
     # sensors/gas/1234567/reading = 1
@@ -89,8 +106,8 @@ def set_last_interval(endpoint_id, consumption) -> None:
     last_reading[endpoint_id] = consumption
 
 
-def get_delta(endpoint_id):
-    return get_last_interval(endpoint_id=endpoint_id)
+def get_delta(endpoint_id, cur_consumption):
+    return cur_consumption - get_last_interval(endpoint_id=endpoint_id)
 
 
 # send data to MQTT broker defined in settings
@@ -108,9 +125,8 @@ if __name__ == '__main__':
     client = docker.from_env()
     rtlamr = client.containers.list(filters={'name': '_rtlamr_'})[0]
     # rtlamr = client.containers.list(filters={'name': '_rtlamr_'})
-    from pprint import pprint
-    pprint(dir(rtlamr))
-    # pprint(len(rtlamr))
+    #from pprint import pprint
+    #pprint(dir(rtlamr))
 
     for line in rtlamr.logs(stream=True):
         process(line)
